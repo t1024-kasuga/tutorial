3f0db70a74c83729bf74813ab4330aa8b9e5c05e
kasuga <kasuga0877@gmail.com>
2020-05-15 01:34:44 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+
+
+  get 'password_resets/new'
+  get 'password_resets/edit'
+  root 'staticpages#home'
+  get  '/help',    to: 'staticpages#help'
+  get  '/about',   to: 'staticpages#about'
+  get  '/contact', to: 'staticpages#contact'
+  get  '/signup',  to: 'users#new'
+  post '/signup',   to: 'users#create'
+  get  '/login',    to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  resources :users do
+   member do
+     get :following, :followers
+   end
+ end
+  resources :account_activations,only:[:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
+end
---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout links" do
+      get root_path
+      assert_template 'staticpages/home'
+      assert_select "a[href=?]", root_path, count: 2
+      assert_select "a[href=?]", help_path
+      assert_select "a[href=?]", about_path
+      assert_select "a[href=?]", contact_path
+      assert_select "a[href=?]", login_path
+      get contact_path
+      assert_select "title",full_title("Contact")
+      get signup_path
+      assert_select "title",full_title("Sign up")
+    end
+
+
+    def setup
+      @user      = users(:michael)
+    end
+
+    test "layout links when logged in" do
+        log_in_as(@user)
+        get root_path
+        assert_template 'staticpages/home'
+        assert_select "a[href=?]", users_path
+        assert_select "a[href=?]", user_path(@user)
+        assert_select "a[href=?]", edit_user_path(@user)
+        assert_select "a[href=?]", logout_path
+        assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
+      end
+   end
